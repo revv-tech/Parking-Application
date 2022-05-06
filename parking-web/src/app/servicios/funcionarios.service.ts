b95ee3d445837a180c65669345f9e8013df8bb80
Marco Reveiz <revvace@gmail.com>
2022-05-06 00:08:54 -0600

    Login

--- parking-web/src/app/servicios/funcionarios.service.ts
+++ parking-web/src/app/servicios/funcionarios.service.ts
@@ -7,8 +7,22 @@ import { Observable } from 'rxjs';
 })
 export class FuncionariosService {
   private baseUrl:string = 'http://localhost:8080/api/funcionarios'
-
+    usuarioLoggeado: any;
+    mail: any;
+    password: any;
     constructor(private http: HttpClient) {
       console.log("Funcionando el servicio de funcionarios")
     }
+
+    login(): Observable<any[]> {
+      console.log("Results:")
+      this.http.get(this.baseUrl+"/login").subscribe(usuario => {
+        this.usuarioLoggeado = usuario
+        console.log(usuario)
+      })
+      return this.usuarioLoggeado;
+    }
+
+    
+
 }
